8faef5c00e1bad1e8f163e365cf1e63e6442f990
Joran <jordanalireed@icloud.com>
2020-11-13 19:01:17 -0500

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,4 +1,26 @@
-describe 'Shoe' do 
+class Shoe
+  attr_accessor :color, :size, :material # remove the attr_accessor for genre
+  attr_reader :brand, :condition #add an attr_reader for genre
+
+  BRANDS = ["Nike", "Addidas", "UnderArmour", "Uggs"]
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+#create the writer for genre and add the logic for the class constant
+  def brand= (brand)
+    @brand = brand
+    BRANDS << brand
+  end
+end
+
+def
+describe()'Shoe' 
   describe 'BRANDS' do
     # Think about it! When does the BRANDS constant need to be
     # made aware of the brands of the shoes that are being created?
